0b615807f1419658032bdde5a0701f32e4605ff1
Felipe Carvalho <felipe@bigdataboutique.com>
2023-11-07 22:25:40 +0000

    fix

--- init.py
+++ init.py
@@ -5,6 +5,11 @@ import sys
 import getopt
 import time
 
+
+def log(msg):
+    print(f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] {msg}")
+
+
 opts, args = getopt.getopt(sys.argv[1:], "hu:p:a:i:x", [
                            "user", "pass", "address", "indicies"])
 params = dict(opts)
@@ -19,7 +24,7 @@ indices_wildcard = "*" if '-i' not in params else params['-i']
 
 repo_rsp = get(f"{es_host}/_snapshot/az_repo", auth=basic, verify=False)
 if repo_rsp.status_code != 200:
-    print("Snapshot repository not found. Creating it...")
+    log("Snapshot repository not found. Creating it...")
 
     repo_body = {
         "type": "azure",
@@ -42,7 +47,7 @@ restore_body = {
     "rename_replacement": "restored-$1"
 }
 
-print(f"Restoring snapshot for {indices_wildcard}...")
+log(f"Restoring snapshot for {indices_wildcard}...")
 
 restore_rsp = post(f"{es_host}/_snapshot/az_repo/snapshot_1/_restore?wait_for_completion=false",
                    auth=basic, verify=False, headers=headers, json=restore_body)
@@ -54,12 +59,12 @@ if restore_rsp.status_code != 200:
 recovery = get(f"{es_host}/_cat/recovery?active_only",
                auth=basic, verify=False)
 while recovery.text != "":
-    print("Restoring is in progress... Recovery response: ", recovery.text)
+    log("Restoring is in progress... Recovery response: ", recovery.text)
     time.sleep(2)
     recovery = get(f"{es_host}/_cat/recovery?active_only",
                    auth=basic, verify=False)
 
-print("Restore completed")
+log("Restore completed")
 
 indices_resp = get(f"{es_host}/restored-*", auth=basic, verify=False)
 if indices_resp.status_code != 200:
@@ -80,23 +85,25 @@ for index_name, payload in indices_payload.items():
 
     new_index_name = index_name.replace('restored-', '')
 
-    print(f"Creating index {new_index_name}...")
+    log(f"Creating index {new_index_name}...")
 
     put_index_resp = put(f"{es_host}/{new_index_name}",
                          auth=basic, verify=False, headers=headers, json=payload)
     if put_index_resp.status_code != 200:
-        print(
+        log(
             f"Failed to create index {new_index_name}. Received status code {put_index_resp.status_code} and body {put_index_resp.json()}")
         continue
-
-    print(f"Waiting for index to be green...")
+    shards_resp = get(f"{es_host}/_cat/shards?pretty",
+                      auth=basic, verify=False)
+    log(f"Shards: {shards_resp.text}")
+    """ log(f"Waiting for index to be green...")
 
     health_resp = get(f"{es_host}/_cluster/health/{new_index_name}?wait_for_status=green&timeout=50s",
                       auth=basic, verify=False)
     if health_resp.status_code != 200:
-        print(
+        log(
             f"Failed to wait for index to be green. Received status code {health_resp.status_code} and body {health_resp.json()}")
-        continue
+        continue """
 
     reindex_body = {
         "source": {"index": index_name},
@@ -104,14 +111,14 @@ for index_name, payload in indices_payload.items():
     }
     # TODO: this is very likely to time out on medium/large indices. You need to add the query param wait_for_completion=false and add a loop that checks if the task id is really done
     # request_per_second=1
-    print(f"Reindexing {index_name} to {new_index_name}...")
+    log(f"Reindexing {index_name} to {new_index_name}...")
     reindex_resp = post(f"{es_host}/_reindex?wait_for_completion=false",
                         auth=basic, verify=False, headers=headers, json=reindex_body)
     # reindex_resp = post(f"{es_host}/_reindex", auth=basic, verify=False, headers=headers, json=reindex_payload)
     reindex_resp_json = reindex_resp.json()
 
     if reindex_resp.status_code != 200:
-        print(
+        log(
             f"Failed to reindex {index_name} to {new_index_name}. Received status code {reindex_resp.status_code} and body {reindex_resp.json()}")
         continue
 
@@ -123,18 +130,18 @@ for index_name, payload in indices_payload.items():
         task_status_resp_json = task_status_resp.json()
         if task_status_resp_json["completed"]:
             break
-        print("Reindexing is in progress... Task status: ",
-              task_status_resp_json)
+        log("Reindexing is in progress... Task status: ",
+            task_status_resp_json)
         time.sleep(5)
 
     if task_status_resp_json["error"] is not None:
-        print(
+        log(
             f"Reindexing failed. Received status code {task_status_resp.status_code} and body {task_status_resp_json}")
         continue
 
-    print("Reindex completed. Deleting source index...")
+    log("Reindex completed. Deleting source index...")
 
     delete_resp = delete(f"{es_host}/{index_name}", auth=basic, verify=False)
     if delete_resp.status_code != 200:
-        print(f"Failed to delete {index_name}")
+        log(f"Failed to delete {index_name}")
         continue
