b4634d98afc56d4c44f6a62e9f839b19ea095b55
Felipe Carvalho <felipe@bigdataboutique.com>
2023-11-07 18:45:17 +0000

    creating the repo if it doesn't exist + logging

--- init.py
+++ init.py
@@ -1,39 +1,62 @@
 from requests import get, post, put, delete
 from requests.auth import HTTPBasicAuth
 
-import sys, getopt
+import sys
+import getopt
 import time
 
-opts, args = getopt.getopt(sys.argv[1:],"hu:p:a:i:x",["user","pass","address","indicies"])
+opts, args = getopt.getopt(sys.argv[1:], "hu:p:a:i:x", [
+                           "user", "pass", "address", "indicies"])
 params = dict(opts)
 
 basic = HTTPBasicAuth(params['-u'], params['-p'])
 headers = {
-"Content-Type": "application/json"
+    "Content-Type": "application/json"
 }
 es_host = f"https://{params['-a']}:9200"
 
 indices_wildcard = "*" if '-i' not in params else params['-i']
 
-############################
-# restoring indices from 
-############################
+repo_rsp = get(f"{es_host}/_snapshot/az_repo", auth=basic, verify=False)
+if repo_rsp.status_code != 200:
+    print("Snapshot repository not found. Creating it...")
 
-restore_bodey = {
-                    "indices": f"{indices_wildcard},-.*",
-                    "ignore_index_settings": "index.search*",
-                    "rename_pattern": "(.+)",
-                    "rename_replacement": "restored-$1"
-                }
+    repo_body = {
+        "type": "azure",
+        "settings": {
+            "container": "snapshots",
+            "readonly": True,
+        }
+    }
 
-restore_rsp = post(f"{es_host}/_snapshot/az_repo/snapshot_1/_restore?wait_for_completion=false", auth=basic, verify=False, headers=headers, json=restore_bodey)
+    repo_rsp = put(f"{es_host}/_snapshot/az_repo",
+                   auth=basic, verify=False, headers=headers, json=repo_body)
 
-recovery = get(f"{es_host}/_cat/recovery?active_only", auth=basic, verify=False)
+    if repo_rsp.status_code != 200:
+        raise Exception("Failed to create repository")
+
+# Restore snapshot
+
+restore_body = {
+    "indices": f"{indices_wildcard},-.*",
+    "ignore_index_settings": "index.search*",
+    "rename_pattern": "(.+)",
+    "rename_replacement": "restored-$1"
+}
+
+print(f"Restoring snapshot for {indices_wildcard}...")
+
+restore_rsp = post(f"{es_host}/_snapshot/az_repo/snapshot_1/_restore?wait_for_completion=false",
+                   auth=basic, verify=False, headers=headers, json=restore_body)
+
+recovery = get(f"{es_host}/_cat/recovery?active_only",
+               auth=basic, verify=False)
 while recovery.text != "":
     print("Recovery response: ", recovery.text)
     time.sleep(2)
-    recovery = get(f"{es_host}/_cat/recovery?active_only", auth=basic, verify=False)
-    
+    recovery = get(f"{es_host}/_cat/recovery?active_only",
+                   auth=basic, verify=False)
+
 # Get all indices + settings
 indices_resp = get(f"{es_host}/restored-*", auth=basic, verify=False)
 
@@ -53,28 +76,34 @@ for index_name, payload in indices_payload.items():
         if field in payload["settings"]["index"]:
             del payload["settings"]["index"][field]
 
-
     # Create the source index
-    new_index_name = index_name.replace('restored-','')
-    put_index_resp = put(f"{es_host}/{new_index_name}", auth=basic, verify=False, headers=headers, json=payload)
+    new_index_name = index_name.replace('restored-', '')
+    print(f"Creating index {new_index_name}...")
+    put_index_resp = put(f"{es_host}/{new_index_name}",
+                         auth=basic, verify=False, headers=headers, json=payload)
 
     if put_index_resp.status_code != 200:
-        print(f"Failed to create indicex {index_name}. Received status code {put_index_resp.status_code}")
+        print(
+            f"Failed to create index {index_name}. Received status code {put_index_resp.status_code}")
         continue
-    
-    # # Reindex with suffix
-    reindex_payload = {"source": {"index": index_name, "size": 1}, "dest": {"index": new_index_name}}
-    print(reindex_payload)
 
+    # # Reindex with suffix
+    reindex_body = {
+        "source": {"index": index_name, "size": 1},
+        "dest": {"index": new_index_name}
+    }
     # TODO: this is very likely to time out on medium/large indices. You need to add the query param wait_for_completion=false and add a loop that checks if the task id is really done
-    #request_per_second=1
-    reindex_resp = post(f"{es_host}/_reindex?wait_for_completion=false&requests_per_second=1", auth=basic, verify=False, headers=headers, json=reindex_payload)
-    #reindex_resp = post(f"{es_host}/_reindex", auth=basic, verify=False, headers=headers, json=reindex_payload)
+    # request_per_second=1
+    print(f"Reindexing {index_name} to {new_index_name}...")
+    reindex_resp = post(f"{es_host}/_reindex?wait_for_completion=false&requests_per_second=1",
+                        auth=basic, verify=False, headers=headers, json=reindex_body)
+    # reindex_resp = post(f"{es_host}/_reindex", auth=basic, verify=False, headers=headers, json=reindex_payload)
     reindex_resp_json = reindex_resp.json()
     print(reindex_resp_json)
 
     if reindex_resp.status_code != 200:
-        print(f"Failed to reindex {index_name} to {new_index_name}. Received status code {reindex_resp.status_code}")
+        print(
+            f"Failed to reindex {index_name} to {new_index_name}. Received status code {reindex_resp.status_code}")
         continue
 
     # # Delete source index
